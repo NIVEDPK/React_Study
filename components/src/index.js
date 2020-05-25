92fa6c7fece89457a8a41a6b86438bfc367c6e6c
NIVEDPK <nivedpkkannan31@gmail.com>
2020-05-25 22:01:07 +0530

    Added new Card and prop communication established

--- components/src/index.js
+++ components/src/index.js
@@ -1,30 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeago="Today at 7:00 AM"
-        comment="Nice Blog post"
-      />
-      <CommentDetail
-        author="Ria"
-        timeago="Today at 2:00 PM"
-        comment="Could be better"
-      />
-      <CommentDetail
-        author="king"
-        timeago="Today at 3:00 AM"
-        comment="Great work"
-      />
-      <CommentDetail
-        author="Mon"
-        timeago="Today at 12:00 AM"
-        comment="Nice Blog "
-      />
+      <ApprovalCard>
+        <div>
+          <h2>Warning!</h2>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeago="Today at 7:00 AM"
+          comment="Nice Blog post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Ria"
+          timeago="Today at 2:00 PM"
+          comment="Could be better"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="king"
+          timeago="Today at 3:00 AM"
+          comment="Great work"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Mon"
+          timeago="Today at 12:00 AM"
+          comment="Nice Blog "
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
