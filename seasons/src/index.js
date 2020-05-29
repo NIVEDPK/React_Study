8cd31bb57b040efa5df35f44481fdfaa0ac49dfc
NIVEDPK <nivedpkkannan31@gmail.com>
2020-05-29 20:18:31 +0530

    using class based component with state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,28 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  let location = {
-    lat: "",
-    long: "",
-  };
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      location.lat = position.coords.latitude;
-      location.long = position.coords.longitude;
-    },
-    (error) => {
-      console.log(error);
-      location = {};
-    }
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: "" };
 
-  return (
-    <div>
-      <div>Latitude : {location.lat}</div>
-      <div>Longitude : {location.long}</div>
-    </div>
-  );
-};
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //this.state.lat = position.coords.latitude;
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
+  }
+
+  //should declare a render method when using react class based component
+  render() {
+    if (!this.state.lat && this.state.errorMessage) {
+      return (
+        <div>
+          <div>Error : {this.state.errorMessage} </div>
+        </div>
+      );
+    }
+
+    if (this.state.lat && !this.state.errorMessage) {
+      return (
+        <div>
+          <div>Latitude : {this.state.lat} </div>
+        </div>
+      );
+    }
+
+    if (!this.state.lat && !this.state.errorMessage) {
+      return (
+        <div>
+          <div>Loading..... </div>
+        </div>
+      );
+    }
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
