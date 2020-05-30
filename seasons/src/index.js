99cdef8d02996122f93664ad8279a8a666bc41f7
NIVEDPK <nivedpkkannan31@gmail.com>
2020-05-30 21:46:40 +0530

    season app finished_ learned default props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,50 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMessage: "" };
+  state = { lat: null, errorMessage: "" };
 
+  componentDidMount() {
     navigator.geolocation.getCurrentPosition(
-      (position) => {
-        //this.state.lat = position.coords.latitude;
-        this.setState({
-          lat: position.coords.latitude,
-        });
-      },
-      (error) => {
-        this.setState({ errorMessage: error.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => this.setState({ errorMessage: error.message })
     );
+  }
+
+  renderContent() {
+    if (!this.state.lat && this.state.errorMessage) {
+      return <ErrorMessage message={this.state.errorMessage} />;
+    }
+
+    if (this.state.lat && !this.state.errorMessage) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please allow location access " />;
   }
 
   //should declare a render method when using react class based component
   render() {
-    if (!this.state.lat && this.state.errorMessage) {
-      return (
-        <div>
-          <div>Error : {this.state.errorMessage} </div>
-        </div>
-      );
-    }
-
-    if (this.state.lat && !this.state.errorMessage) {
-      return (
-        <div>
-          <div>Latitude : {this.state.lat} </div>
-        </div>
-      );
-    }
-
-    if (!this.state.lat && !this.state.errorMessage) {
-      return (
-        <div>
-          <div>Loading..... </div>
-        </div>
-      );
-    }
+    return <div>{this.renderContent()}</div>;
   }
 }
-
 ReactDOM.render(<App />, document.querySelector("#root"));
