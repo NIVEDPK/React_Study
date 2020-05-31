70ff6e7b2c359f8d361b477039b4fa578f8b4c4e
NIVEDPK <nivedpkkannan31@gmail.com>
2020-05-31 22:38:36 +0530

    adding axios configuration

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import axios from "axios";
 import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = { image: [] };
   onSubmit = async (term) => {
-    const response = await axios.get("search/photos", {
+    const response = await unsplash.get("search/photos", {
       params: {
         query: term,
       },
